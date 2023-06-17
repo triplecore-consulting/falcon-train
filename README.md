4467a91848371aeaa4630c2db2f0720f0279e105
HC Pahlig <hc@triplecore.io>
2023-06-17 08:54:51 +0200

    readme, requirements, setup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # falcon-train
+
+## Log into machine
+
+-   triplecore aws login (`awslogintriplecore <MFA>` for me)
+-   `aws ssm start-session --target <InstanceID>`
+
+## get repo
+
+-   `git clone https://github.com/triplecore-consulting/falcon-train.git`
+-   `cd falcon-train`
+
+## run setup
+
+-   `bash ./setup.sh`
+-   `LD_LIBRARY_PATH` is added to `.bashrc` Restart bash `exec bash` or
+    logout/login
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LD_LIBRARY_PATH="/opt/amazon/efa/lib:/opt/amazon/openmpi/lib:/usr/local/cuda/efa/lib:/usr/local/cuda/lib:/usr/local/cuda/lib64:/usr/local/cuda:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda/targets/x86_64-linux/lib:/usr/local/lib:/usr/lib"
+echo 'export LD_LIBRARY_PATH="/opt/amazon/efa/lib:/opt/amazon/openmpi/lib:/usr/local/cuda/efa/lib:/usr/local/cuda/lib:/usr/local/cuda/lib64:/usr/local/cuda:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda/targets/x86_64-linux/lib:/usr/local/lib:/usr/lib"' >>~/.bashrc
+
+sudo apt install python3.8-venv
+
+python3 -m venv venv
+
+pip install -U -r requirements.txt
